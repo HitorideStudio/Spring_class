6e5814e02b31498984ad7b04ab2d261614098b5b
HitorideStudio <mfboy19@gmail.com>
2019-08-30 17:41:58 +0900

    팀프로젝트 연습

---
+++ project_2/src/project_2/bean/GoBean.java
@@ -0,0 +1,20 @@
+package project_2.bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/finder/")
+public class GoBean {
+	
+	@RequestMapping("main.go")
+	public String main() {
+		return "/finder/main";
+	}
+	@RequestMapping("map.go")
+	public String map() {
+		return "/finder/map";
+	}
+
+}
